a19c00c35b57eb4cac6c3c20162a2987d78fd26c
NOT-BAD-YAR <727724eucy032@skcet.ac.in>
2026-07-23 22:09:27 +0530

    inbanaturals added & backend completed

--- platform/backend/database.py
+++ platform/backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Ks%40kbd23777@localhost/pwnchain"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:toor@localhost/pwnchain"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
